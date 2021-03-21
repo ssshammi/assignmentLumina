a897b93637b23b7e0779d36a1c27166cd88439c2
shammi <ssshammi@hotmail.com>
2021-03-21 20:06:35 +0000

    async not working
    async inside loop not working, will try map

--- db/moviedb.js
+++ db/moviedb.js
@@ -1,20 +1,39 @@
-var knex = require('knex')({
+const knex = require('knex')({
     client: 'sqlite3',
     connection: {
         filename: './db/moviedb.sqlite3'
     }
 });
-
-
-knex.select("*").from("users")
+ 
+ function  getUsers () {
+    return  knex("users").select("*");
+/*knex.select("*").from("users")
     .then(function (users) {
+        return users;
         users.forEach((user) => { //use of Arrow Function
             console.log({ ...user });
         });
     }).catch(function (err) {
         // All the error can be checked in this piece of code  
         console.log(err);
+        return {};
     }).finally(function () {
         // To close the connection pool  
         knex.destroy();
     });
+*/
+};
+
+function addUser(user) {
+    return knex("users").insert(user);
+};
+
+function updateUserMovie(Id, f_movies){
+    return knex("users").where({ id: Id }).update({ favourite_movies: f_movies })
+};
+
+module.exports = {
+    getUsers,
+    addUser,
+    updateUserMovie
+};
--- index.js
+++ index.js
@@ -1,48 +1,95 @@
-const express = require('express');
-const needle = require('needle');
+const express = require("express");
+const needle = require("needle");
 const port = process.env.PORT || 8080;
 const App = express();
-const omdbURL = 'http://www.omdbapi.com/';
-let param ={
-    apikey: '6694fd2f'
+const omdbURL = "http://www.omdbapi.com/";
+let param = {
+  apikey: "6694fd2f",
 };
 
+const moviedb = require("./db/moviedb");
 
-App.get('/', (request,response) =>{
+App.get("/", async (request, response) => {
+  var randomMovie = {};
+  console.log("nothing");
 
-    console.log("nothing");
-    
-    param.i = 'tt3896198';
-
-    needle.request('get', omdbURL, param, function (err, res, res) {
-        if (err) {
-            console.log("err: " + err);
-            return;
-        }
-        console.log(res);
-        return;
+  param.i = "tt3896198";
+  randomMovie = await needle("get", omdbURL, param, { json: false })
+    .then(function (res) {
+      return res.body;
+    })
+    .catch(function (err) {
+      console.log("Error " + err);
     });
-    response.send("see console");
-    response.end();
+
+  console.log(randomMovie.Title);
+  response.send("see console" + randomMovie.Title);
 });
 
+App.get("/users", async (request, response) => {
+  //console.log("movieDB");
 
-App.listen(port , ()=>{
+  const userlist = await moviedb.getUsers();
+  // console.log(userlist);
+  var userMovieList = [];
+  userMovieList = Object.keys(userlist).forEach(function (user) {
+    userMovieList.push({
+      id: userlist[user].id,
+      name: userlist[user].firstName + " " + userlist[user].lastName,
+      favourite_movies: getMovieData(userlist[user].favourite_movies),
+    });
+  });
 
-    console.log(`Listening to port :${port}  `);
+  console.log(userMovieList);
+  response.send("see console" + userMovieList);
+  response.end();
+});
+
+async function getMovieData(favourite_movies) {
+  var favMovies = [];
+  var movieArray = favourite_movies.split(",");
+  var itemsProcessed = 0;
+
+  let movieslist = await movieArray.forEach(async function (movieID) {
+    param.i = movieID;
+
+    let MovieDetail = await needle("get", omdbURL, param, { json: false })
+      .then(function (res) {
+        return res.body;
+      })
+      .catch(function (err) {
+        console.log("Error " + err);
+      });
+
+    favMovies.push({
+      ID: MovieDetail.imdbID,
+      Title: MovieDetail.Title,
+      Year: MovieDetail.Year,
+      Plot: MovieDetail.Plot,
+      Poster: MovieDetail.Poster,
+    });
+    //console.log(favMovies);
+    return;
+  });
+  movieslist
+  console.log(favMovies);
+  return favMovies;
+
+}
+
+App.listen(port, () => {
+  console.log(`Listening to port :${port}  `);
 });
 //database
 
-
-///client api 
+///client api
 //OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=6694fd2f
-    //key: 6694fd2f
+//key: 6694fd2f
 //users and movie list
-    //movies list keywork, year
-    // search movie http://www.omdbapi.com/?s=Toxic&type=movie&page=2&apikey=6694fd2f
-//login user 
-    //my movie
-        //remove movie
-    // movies list by keyword, year 
-        //add movie
-       
+//movies list keywork, year
+// search movie http://www.omdbapi.com/?s=Toxic&type=movie&page=2&apikey=6694fd2f
+//login user
+//my movie
+//remove movie
+// movies list by keyword, year
+//add movie
